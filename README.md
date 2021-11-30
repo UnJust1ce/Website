1720fe7f969e8fac332d7971fd75633cf88a1bf7
Tane Bosman <40045439+UnJust1ce@users.noreply.github.com>
2021-11-30 17:31:07 +1300

    e

--- README.md
+++
@@ -1,2 +0,0 @@
-# Website
-The website at https://fastforward.team/
---
+++ php/head.php
@@ -0,0 +1,13 @@
+    <div>
+      <nav>
+        <a href="\">FastForward</a>
+        <a href="\">Home</a>
+      </nav>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <a href="\">Home</a>
+        </li>
+      </ul>
+    </nav>
