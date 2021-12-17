53ee8e966adb1767ff270f464053839492acd787
TexRexTex <tanebosman@gmail.com>
2021-12-17 17:14:14 +1300

    Navbar javascript
    Setup js for getting the navbar to work with smaller devices.

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,29 @@ for(i = 0; i < dropdowns.length; i++) {
         this.parentElement.classList.toggle("expanded")
     });
 }
+
+
+document.querySelector("button.bars").addEventListener("click", function() {
+  document.querySelector("header").classList.toggle("dropdowndisplay");
+  if(document.querySelector("header").classList.contains("dropdowndisplay")){
+    setTimeout(function(){
+      document.querySelector(".nav-ul").classList.toggle("dropdowndisplay");
+      document.querySelector(".nav").classList.toggle("dropdowndisplay");
+    }, 50)
+  } else {
+    document.querySelector(".nav-ul").classList.toggle("dropdowndisplay");
+    document.querySelector(".nav").classList.toggle("dropdowndisplay");
+  }
+});
+
+
+function check(media) {
+  if (media.matches) {
+    document.body.children[0].classList.add("small")
+  } else {
+   document.body.children[0].classList.remove("small")
+  }
+}
+minSize = window.matchMedia("(max-width: 1002px)")
+check(minSize)
+minSize.addListener(check)
