6b55c8bedc0fd6ae019fe9150d4d4f1c8c8bc91e
Tane Bosman <40045439+UnJust1ce@users.noreply.github.com>
2022-02-22 08:23:15 +1300

    add the button and the code

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,3 +31,42 @@ function check(media) {
 minSize = window.matchMedia("(max-width: 1002px)")
 check(minSize)
 minSize.addListener(check)
+
+function browser() {
+	u = navigator.userAgent;
+	return u.indexOf("Firefox") > -1 ? "Firefox" : u.indexOf("OP") > -1 ? "Opera" : u.indexOf("Edg") > -1 ? "Edge" : u.indexOf("Chrome") > -1 ? "Chrome" : !!window.chrome ? "Chromium based browsers" : null
+}
+
+//add get fastforward for browser button
+b = browser();
+btn = document.querySelector("a.btn.ff");
+switch (b) {
+  case "Firefox":
+    btn.classList.add("firefox");
+    btn.innerText += " for Firefox";
+    btn.href="https://addons.mozilla.org/en-US/firefox/addon/fastforwardteam/";
+    break;
+  case "Chrome":
+    btn.classList.add("chrome");
+    btn.innerText += " for Chrome";
+    btn.href="https://chrome.google.com/webstore/detail/fastforward/icallnadddjmdinamnolclfjanhfoafe";
+    break;
+  case "Edge":
+    btn.classList.add("edge");
+    btn.innerText += " for Edge";
+    btn.href="https://microsoftedge.microsoft.com/addons/detail/fastforward/ldcclmkclhomnpcnccgbgleikchbnecl";
+    break;
+  case "Opera":
+    btn.classList.add("opera");
+    btn.innerText += " for Opera";
+    btn.href="https://chrome.google.com/webstore/detail/fastforward/icallnadddjmdinamnolclfjanhfoafe";
+    break;
+  case "Chromium based browsers":
+    btn.classList.add("chromium");
+    btn.innerText += " for Chromium based browsers";
+    btn.href="https://chrome.google.com/webstore/detail/fastforward/icallnadddjmdinamnolclfjanhfoafe";
+    break;
+  default:
+    btn.classList.add("default");
+    btn.href="/install.html";
+}
